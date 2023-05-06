2e9aeb8e07cfc32ec414e23ca20d8f8ce4d49c92
SkywalkerERR <shura.pro.05@mail.ru>
2023-05-06 17:27:05 +0500

    second commit

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import itertools
 
 words = []
 
-with open('words-pool.txt','r', encoding='UTF-8') as f:
-    for line in f.readlines():
-        if '\n' in line:
-            line = line[:-1]
-        words.append(line)
+def main():
+    with open('words-pool.txt','r', encoding='UTF-8') as f:
+        for line in f.readlines():
+            if '\n' in line:
+                line = line[:-1]
+            words.append(line)
 
 
-permutations = itertools.permutations
+    permutations = itertools.permutations
 
-for i in permutations(words, 12):
-    print(i)
+    for i in permutations(words, 12):
+        print(i)
 
+if __name__ == "__main__":
+    main()
